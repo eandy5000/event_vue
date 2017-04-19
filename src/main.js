c4728897dc4ad0824278fe42179c7560b74429c4
Andy Eccleston <eandy5000@yahoo.com>
2017-04-19 13:29:09 -0500

    added root view and two components

--- src/main.js
+++ src/main.js
@@ -3,7 +3,37 @@ import Vue from 'vue';
 
 new Vue({
     el: '#app',
-    data: {
-        msg: "hi there"
+    components: {
+        'movie-list': {
+            template: `
+            <div id="movie-list">
+            Movie List 
+                <div v-for="movie in movies" class="movie">
+                    {{ movie.title }}
+                </div>
+            </div>`,
+            data() {
+               return {
+                 movies: [
+                    {title: 'one'},
+                    {title: 'two'},
+                    {title: 'three'},
+                    {title: 'four'}
+                ]
+               }
+            }
+        },
+        'movie-filter': {
+            template: `
+            <div id="movie-filter">
+            Movie filter
+            </div>
+            `,
+            components: {
+                template: `
+                
+                `
+            }
+        }
     }
-})
+});
