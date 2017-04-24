0126e64e38d3e8d6c325df3827d2606f95cf9bfd
Andy Eccleston <eandy5000@yahoo.com>
2017-04-24 10:58:48 -0500

    emitting custom event to root and filting movie-list by genre

--- src/main.js
+++ src/main.js
@@ -4,32 +4,69 @@ import genres from './util/genres';
 
 new Vue({
     el: '#app',
+    methods: {
+        checkFilter: function(category, title, checked) {
+            if(checked) {
+                this[category].push(title);
+            } else {
+                let index = this[category].indexOf(title);
+                if (index > -1) {
+                    this[category].splice(index, 1);
+                }
+            }
+        }
+    },
+    data: {
+        genre: [],
+        time: []
+    },
     components: {
         'movie-list': {
             template: `
             <div id="movie-list">
             Movie List 
-                <div v-for="movie in movies" class="movie">
+                <div v-for="movie in filteredMovies" class="movie">
                     {{ movie.title }}
                 </div>
             </div>`,
+            props: ['genre', 'time'],
+            methods: {
+                movieGenreFilter(movie) {
+                    if(!this.genre.length) {
+                        return true;
+                    } else {
+                        return this.genre.find(foo => {return movie.genre === foo});
+                    }
+                }
+            },
+            computed: {
+                filteredMovies() {
+                        return this.movies.filter(this.movieGenreFilter);
+                    }
+            },
             data() {
                return {
                  movies: [
-                    {title: 'one'},
-                    {title: 'two'},
-                    {title: 'three'},
-                    {title: 'four'}
+                    {title: 'Die Hard', genre: genres.ACTION},
+                    {title: 'Home Alone', genre: genres.ADVENTURE},
+                    {title: 'Three Men and a Baby', genre: genres.COMEDY},
+                    {title: 'Wall-E', genre: genres.FAMILY}
                 ]
                }
             }
         },
+
         'movie-filter': {
             template: `
             <div id="movie-filter">
             <h2>Filter Results</h2>
                 <div class="filter-group">  
-                    <check-filter v-for="genre in genres" v-bind:title="genre"></check-filter>
+                    <check-filter 
+                        v-for="genre in genres" 
+                        v-bind:title="genre"
+                        v-on:check-filter="checkFilter"
+                    >
+                    </check-filter>
                 </div>
             </div>
             `,
@@ -38,7 +75,7 @@ new Vue({
                     template: `
                     <div 
                         v-bind:class="{'check-filter': true, 'active': checked }" 
-                        v-on:click="checked = !checked"
+                        v-on:click="checkFilter"
                     >
                         <span class="checkbox"></span>
                         <span class="check-filter-title">{{title}}</span>
@@ -49,11 +86,23 @@ new Vue({
                         return {
                             checked: false
                         };
+                    },
+                    methods: {
+                        checkFilter: function() {
+                            this.checked = !this.checked;
+                            this.$emit('check-filter', 'genre', this.title, this.checked);
+                        }
                     }
                 }
             },
+
             data() {
                 return { genres };
+            },
+            methods: {
+                checkFilter: function(category, title, checked) {
+                    this.$emit('check-filter',category, title, checked);
+                }
             }
         }
     }
